1afe77c16472f7da95bbd763b9c0ef76f14865df
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-21 22:51:40 +0900

    init graphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+const link = createHttpLink({
+  uri: "http://localhost:3000/graphql",
+  credentials: "include",
+});
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link,
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
