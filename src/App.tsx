3e1345dbbe81de61d3386d8530074ea8db669e52
Tomoya Hayashi <tomoya128@gmail.com>
2023-06-27 19:48:56 +0900

    form箇所にtailwindを適応

--- src/App.tsx
+++ src/App.tsx
@@ -14,25 +14,44 @@ function App() {
   const [deleteBook] = useDeleteBookMutation({ refetchQueries: ["books"] });
 
   return (
-    <div className="container mx-auto">
-      <h1 className="text-3xl font-bold underline">CRUD books</h1>
-      <label className="block">
-        <span className="text-gray-700">title</span>
-        <input className="form-input mt-1 block w-full" placeholder="book title" value={title} onChange={(e) => setTitle(e.target.value)} />
-      </label>
-      <label className="block">
-        <span className="text-gray-700">discription</span>
-        <textarea className="form-textarea mt-1 block w-full" placeholder="book discription" value={discription} onChange={(e) => setDiscription(e.target.value)} />
-      </label>
-      <button
-        onClick={() => {
-          createBook({ variables: { params: { title: title, discription: discription } } });
-          setTitle("");
-          setDiscription("");
-        }}
-      >
-        保存
-      </button>
+    <div>
+      <h1 className="text-3xl font-bold underline">本の情報を追加する</h1>
+      <form className="w-full max-w-sm">
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              タイトル
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" placeholder="book title" value={title} onChange={(e) => setTitle(e.target.value)} />
+          </div>
+        </div>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
+              概要
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500" placeholder="book title" value={discription} onChange={(e) => setDiscription(e.target.value)} />
+          </div>
+        </div>  
+        <div className="md:flex md:items-center">
+          <div className="md:w-1/3"></div>
+          <div className="md:w-2/3">
+            <button className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              onClick={() => {
+                createBook({ variables: { params: { title: title, discription: discription } } });
+                setTitle("");
+                setDiscription("");
+              }}
+            >
+              本の情報を保存する
+            </button>
+          </div>
+        </div>
+      </form>
       <table className="border-collapse border border-slate-500 ...">
         <thead>
           <tr>
